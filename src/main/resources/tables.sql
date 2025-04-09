1d9e9e618085915058442ae548f96474f82243cc
rokuban <118823402+rokuban@users.noreply.github.com>
2025-04-09 22:27:54 +0200

    Update SQL tables

--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS user CASCADE;
 DROP TABLE IF EXISTS track CASCADE;
 DROP TABLE IF EXISTS user_tracks CASCADE;
 DROP TABLE IF EXISTS playlist CASCADE;
+DROP TABLE IF EXISTS playlist_tracks CASCADE;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
@@ -19,6 +20,7 @@ CREATE TABLE user
 
 CREATE TABLE track
 (
+    track_id    integer      not null,
     title       varchar(16)  not null,
     image_path  varchar(128) not null,
     album_title varchar(16)  not null,
@@ -27,29 +29,38 @@ CREATE TABLE track
     genre       varchar(16),
     path        varchar(16),
 
-    primary key (title, artist),
+    primary key (track_id),
     check (genre in ('classical', 'rock', 'edm'))
 );
 
 CREATE TABLE user_tracks
 (
     nickname varchar(32) not null,
-    title    varchar(16) not null,
-    artist   varchar(16) not null,
+    track_id integer     not null,
 
-    primary key (nickname, title, artist),
+    primary key (nickname, track_id),
     foreign key (nickname) references user (nickname),
-    foreign key (title, artist) references track (title, artist)
+    foreign key (track_id) references track (track_id)
 );
 
 CREATE TABLE playlist
 (
-    id             integer auto_increment,
+    playlist_id    integer     not null,
     playlist_title varchar(16) not null,
-    title          varchar(16) not null,
-    artist         varchar(16) not null,
-    track_order    integer,
+    creation_date  date        not null,
+    nickname       varchar(32) not null,
 
-    primary key (id, title, artist),
-    foreign key (title, artist) references user_tracks (title, artist)
+    primary key (playlist_id),
+    foreign key (nickname) references user (nickname)
+);
+
+CREATE TABLE playlist_tracks
+(
+    playlist_id  integer not null,
+    track_id     integer not null,
+    custom_order integer,
+
+    primary key (playlist_id, track_id),
+    foreign key (playlist_id) references playlist (playlist_id),
+    foreign key (track_id) references track (track_id)
 );
