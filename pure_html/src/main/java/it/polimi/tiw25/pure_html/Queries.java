930c271f329da1f08885b8761ef4186e9c394fa1
rokuban <118823402+rokuban@users.noreply.github.com>
2025-04-17 10:06:44 +0200

    Fix CHECK_USER query

--- pure_html/src/main/java/it/polimi/tiw25/pure_html/Queries.java
+++ pure_html/src/main/java/it/polimi/tiw25/pure_html/Queries.java
@@ -5,7 +5,7 @@ package it.polimi.tiw25.pure_html;
  */
 public class Queries {
     // Checks
-    public static final String CHECK_USER = "SELECT * FROM user WHERE nickname = ?";
+    public static final String CHECK_USER = "SELECT * FROM user WHERE nickname = ? AND password = ?";
 
     // Insertions
     public static final String ADD_USER = "INSERT INTO user (nickname, password, name, surname) VALUES (?, ?, ?, ?)";
